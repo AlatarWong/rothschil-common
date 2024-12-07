a770beb23cc097f0a65d5f77551124136e677000
Atlas <WCNGS@QQ.COM>
2024-12-07 16:58:10 +0800

    去除冗余代码

--- src/main/java/io/github/rothschil/common/intf/IntfConfEntity.java
+++ src/main/java/io/github/rothschil/common/intf/IntfConfEntity.java
@@ -1,9 +1,5 @@
 package io.github.rothschil.common.intf;
 
-// import com.baomidou.mybatisplus.annotations.TableField;
-// import com.baomidou.mybatisplus.annotations.TableId;
-// import com.baomidou.mybatisplus.annotations.TableName;
-// import com.baomidou.mybatisplus.enums.IdType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
