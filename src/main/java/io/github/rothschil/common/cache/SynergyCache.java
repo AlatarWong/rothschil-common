f752fe8e09707d6f5ba5948c5d650f20805a55e6
Sam <WCNGS@HOTMAIL.COM>
2025-01-07 17:09:23 +0800

    NONE

--- src/main/java/io/github/rothschil/common/cache/SynergyCache.java
+++ src/main/java/io/github/rothschil/common/cache/SynergyCache.java
@@ -1,15 +1,23 @@
 package io.github.rothschil.common.cache;
 
-import io.github.rothschil.common.cache.redis.CustomizedRedisCache;
+
+import com.alibaba.fastjson.JSON;
+import io.github.rothschil.common.cache.enums.ChannelTopicEnum;
+import io.github.rothschil.common.cache.listener.RedisPublisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.Cache;
 import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.cache.support.AbstractValueAdaptingCache;
 import org.springframework.cache.support.NullValue;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.core.RedisOperations;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.Callable;
 
 
@@ -26,10 +34,13 @@ public class SynergyCache extends AbstractValueAdaptingCache {
      */
     private boolean usedFirstCache = true;
 
-    /**
-     * redis缓存
-     */
-    private final CustomizedRedisCache redisCache;
+    private RedisCacheWriter cacheWriter;
+
+    private RedisCacheConfiguration cacheConfig;
+
+    private RedisCacheManager redisCacheManager;
+
+    private Cache cache;
 
     /**
      * Caffeine缓存
@@ -39,9 +50,10 @@ public class SynergyCache extends AbstractValueAdaptingCache {
     RedisOperations<? extends Object, ? extends Object> redisOperations;
 
     /**
+     *
+     * String name, RedisCacheWriter cacheWriter, RedisCacheConfiguration cacheConfig
      * @param name              缓存名称
      * @param prefix            缓存前缀
-     * @param redisOperations   操作Redis的RedisTemplate
      * @param expiration        redis缓存过期时间
      * @param preloadSecondTime redis缓存自动刷新时间
      * @param allowNullValues   是否允许存NULL，默认是false
@@ -49,15 +61,17 @@ public class SynergyCache extends AbstractValueAdaptingCache {
      * @param forceRefresh      是否强制刷新（走数据库），默认是false
      * @param caffeineCache     Caffeine缓存
      */
-    public SynergyCache(String name, byte[] prefix, RedisOperations<? extends Object, ? extends Object> redisOperations,
-                         long expiration, long preloadSecondTime, boolean allowNullValues, boolean usedFirstCache,
-                         boolean forceRefresh, com.github.benmanes.caffeine.cache.Cache<Object, Object> caffeineCache) {
+    public SynergyCache(String name, byte[] prefix, RedisCacheWriter cacheWriter, RedisCacheConfiguration cacheConfig,
+                        long expiration, long preloadSecondTime, boolean allowNullValues, boolean usedFirstCache,
+                        boolean forceRefresh, com.github.benmanes.caffeine.cache.Cache<Object, Object> caffeineCache) {
 
         super(allowNullValues);
         this.name = name;
+        this.cacheWriter=cacheWriter;
+        this.cacheConfig=cacheConfig;
         this.usedFirstCache = usedFirstCache;
         this.redisOperations = redisOperations;
-        this.redisCache = new CustomizedRedisCache(name, prefix, redisOperations, expiration, preloadSecondTime, forceRefresh, allowNullValues);
+        this.cache = RedisCacheManager.builder(cacheWriter).build().getCache(name);
         this.caffeineCache = new CaffeineCache(name, caffeineCache, allowNullValues);
     }
 
@@ -71,9 +85,6 @@ public class SynergyCache extends AbstractValueAdaptingCache {
         return this;
     }
 
-    public CustomizedRedisCache getSecondaryCache() {
-        return this.redisCache;
-    }
 
     public CaffeineCache getFirstCache() {
         return this.caffeineCache;
@@ -90,7 +101,7 @@ public class SynergyCache extends AbstractValueAdaptingCache {
 
         if (wrapper == null) {
             // 查询二级缓存
-            wrapper = redisCache.get(key);
+            wrapper = Objects.requireNonNull(cache).get(key);
             caffeineCache.put(key, wrapper == null ? null : wrapper.get());
             logger.debug("查询二级缓存,并将数据放到一级缓存。 key:{},返回值是:{}", key, JSON.toJSONString(wrapper));
         }
@@ -108,7 +119,8 @@ public class SynergyCache extends AbstractValueAdaptingCache {
 
         if (value == null) {
             // 查询二级缓存
-            value = redisCache.get(key, type);
+            value = Objects.requireNonNull(cache).get(key,type);
+//            value = redisCache.get(key, type);
             caffeineCache.put(key, value);
             logger.debug("查询二级缓存,并将数据放到一级缓存。 key:{},返回值是:{}", key, JSON.toJSONString(value));
         }
@@ -138,7 +150,8 @@ public class SynergyCache extends AbstractValueAdaptingCache {
         if (usedFirstCache) {
             caffeineCache.put(key, value);
         }
-        redisCache.put(key, value);
+        cache.put(key,value);
+//        redisCache.put(key, value);
     }
 
     @Override
@@ -146,13 +159,13 @@ public class SynergyCache extends AbstractValueAdaptingCache {
         if (usedFirstCache) {
             caffeineCache.putIfAbsent(key, value);
         }
-        return redisCache.putIfAbsent(key, value);
+        return cache.putIfAbsent(key, value);
     }
 
     @Override
     public void evict(Object key) {
         // 删除的时候要先删除二级缓存再删除一级缓存，否则有并发问题
-        redisCache.evict(key);
+        cache.evict(key);
         if (usedFirstCache) {
             // 删除一级缓存需要用到redis的Pub/Sub（订阅/发布）模式，否则集群中其他服服务器节点的一级缓存数据无法删除
             Map<String, Object> message = new HashMap<>();
@@ -167,7 +180,7 @@ public class SynergyCache extends AbstractValueAdaptingCache {
 
     @Override
     public void clear() {
-        redisCache.clear();
+        cache.clear();
         if (usedFirstCache) {
             // 清除一级缓存需要用到redis的订阅/发布模式，否则集群中其他服服务器节点的一级缓存数据无法删除
             Map<String, Object> message = new HashMap<>();
@@ -187,7 +200,7 @@ public class SynergyCache extends AbstractValueAdaptingCache {
             logger.debug("查询一级缓存 key:{},返回值是:{}", key, JSON.toJSONString(value));
         }
         if (value == null) {
-            value = redisCache.get(key);
+            value = cache.get(key);
             logger.debug("查询二级缓存 key:{},返回值是:{}", key, JSON.toJSONString(value));
         }
         return value;
@@ -201,7 +214,7 @@ public class SynergyCache extends AbstractValueAdaptingCache {
      * @return
      */
     private <T> Object getForSecondaryCache(Object key, Callable<T> valueLoader) {
-        T value = redisCache.get(key, valueLoader);
+        T value = cache.get(key, valueLoader);
         logger.debug("查询二级缓存 key:{},返回值是:{}", key, JSON.toJSONString(value));
         return toStoreValue(value);
     }
--- src/main/java/io/github/rothschil/common/cache/redis/CustomizedRedisCache.java
+++ src/main/java/io/github/rothschil/common/cache/redis/CustomizedRedisCache.java
@@ -1,28 +1,41 @@
-package io.github.rothschil.common.cache.redis;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.redis.cache.RedisCache;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheWriter;
-
-/**
- * 自定义的redis缓存
- *
- * @author yuhao.wang
- */
-public class CustomizedRedisCache extends RedisCache {
-
-    private static final Logger logger = LoggerFactory.getLogger(CustomizedRedisCache.class);
-
-    /**
-     * Create new {@link RedisCache}.
-     *
-     * @param name        must not be {@literal null}.
-     * @param cacheWriter must not be {@literal null}.
-     * @param cacheConfig must not be {@literal null}.
-     */
-    protected CustomizedRedisCache(String name, RedisCacheWriter cacheWriter, RedisCacheConfiguration cacheConfig) {
-        super(name, cacheWriter, cacheConfig);
-    }
-}
+//package io.github.rothschil.common.cache.redis;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.data.redis.cache.RedisCache;
+//import org.springframework.data.redis.cache.RedisCacheConfiguration;
+//import org.springframework.data.redis.cache.RedisCacheWriter;
+//
+///**
+// * 自定义的redis缓存
+// *
+// * @author yuhao.wang
+// */
+//public class CustomizedRedisCache extends RedisCache {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(CustomizedRedisCache.class);
+//
+//    /**
+//     * Create new {@link RedisCache}.
+//     *
+//     * @param name        must not be {@literal null}.
+//     * @param cacheWriter must not be {@literal null}.
+//     * @param cacheConfig must not be {@literal null}.
+//     */
+//    protected CustomizedRedisCache(String name, RedisCacheWriter cacheWriter, RedisCacheConfiguration cacheConfig) {
+//        super(name, cacheWriter, cacheConfig);
+//    }
+//
+//    @Override
+//    public ValueWrapper get(Object key) {
+//        return super.get(key);
+//    }
+//
+//    @Override
+//    public <T> T get(Object key, Class<T> type) {
+//        return super.get(key, type);
+//    }
+//
+//
+//
+//}
